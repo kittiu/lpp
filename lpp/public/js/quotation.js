8cefd4ef7830921dac4b9a83123f0e90c70e9e68
daris-dev <daris@goingjesse.com>
2024-08-15 10:52:33 +0000

    [Edited]: Quotation-ที่หน้า search กรองด้วยประเภทไอเทม customer group ได้

--- lpp/public/js/quotation.js
+++ lpp/public/js/quotation.js
@@ -3,3 +3,13 @@ frappe.ui.form.on("Quotation", {
         frm.set_value("valid_till", frappe.datetime.add_months(frm.doc.transaction_date, 12));
     },
 });
+/*
+    frappe.ui.form.on('Payment Schedule', {
+        payment_term(frm, cdt, cdn) {
+            let row = frappe.get_doc(cdt, cdn);
+            row.invoice_portion = 100;
+            
+            frm.refresh_field('payment_schedule');
+        }
+    })
+*/
