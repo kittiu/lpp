5e25c241b62134a749e17ff1ffbfe0f4680a4f61
yot-anan-gj <yot-anan@goingjesse.com>
2024-09-19 04:40:31 +0000

    [Added] : Refactor purchase billing code and add new functionality
    - Refactor the class name from CustomSalesBilling to CustomPurchaseBilling in custom_purchase_billing.py
    - Add a new method get_purchase_invoices_in_month to retrieve purchase invoices based on supplier, currency, threshold type, threshold date, and tax type in custom_purchase_billing.py
    - Update the refresh function in purchase_billing.js to call the new method get_purchase_invoices

--- lpp/custom/custom_purchase_billing.py
+++ lpp/custom/custom_purchase_billing.py
@@ -1,7 +1,8 @@
 import frappe
 from purchase_billing.purchase_billing.doctype.purchase_billing.purchase_billing import PurchaseBilling
+from frappe.utils import get_first_day, get_last_day
 
-class CustomSalesBilling(PurchaseBilling):      
+class CustomPurchaseBilling(PurchaseBilling):      
     @frappe.whitelist()
     def make_journal_entry(self):
         # สร้าง Journal Entry 
@@ -16,3 +17,36 @@ class CustomSalesBilling(PurchaseBilling):
             journal_entry.due_date = purchase_billing.threshold_date
 
         return journal_entry
+    
+    @frappe.whitelist()
+    def get_purchase_invoices_in_month(self, supplier, currency, threshold_type, threshold_date, tax_type = None):
+        if not (supplier, currency, tax_type, threshold_date):
+            return {}
+
+        filters = {
+            "supplier": supplier,
+            "currency": currency,
+            "docstatus": 1,
+            "outstanding_amount": [">", 0],
+        }
+
+        if tax_type:
+            filters["taxes_and_charges"] = tax_type
+
+        # Calculate the first and last day of the month for the given threshold_date
+        first_day_of_month = get_first_day(threshold_date)
+        last_day_of_month = get_last_day(threshold_date)
+
+        # Apply the posting or due date condition based on threshold_type
+        if threshold_type == "Due Date":
+            filters["posting_date"] = ["between", [first_day_of_month, last_day_of_month]]
+        if threshold_type == "Invoice Date":
+            filters["due_date"] = ["between", [first_day_of_month, last_day_of_month]]
+
+        invoices = frappe.get_list(
+            "Purchase Invoice",
+            filters=filters,
+            pluck="name",
+        )
+
+        return invoices
--- lpp/public/js/purchase_billing.js
+++ lpp/public/js/purchase_billing.js
@@ -1,18 +1,45 @@
 frappe.ui.form.on('Purchase Billing', {
-    refresh: function(frm) {
-        frm.add_custom_button(__('Create Journal Entry'), function() {
+    refresh: function (frm) {
+        frm.add_custom_button(__('Create Journal Entry'), function () {
             make_journal_entry(frm);
         });
+    },
+    get_purchase_invoices: function (frm) {
+        
+        if (frm.doc.threshold_date) {
+            return frm.call({
+                method: "get_purchase_invoices_in_month",
+                doc: frm.doc,
+                args: {
+                    supplier: frm.doc.supplier,
+                    currency: frm.doc.currency,
+                    tax_type: frm.doc.tax_type,
+                    threshold_type: frm.doc.threshold_type,
+                    threshold_date: frm.doc.threshold_date
+                },
+                callback: function (r) {
+                    console.log("get_purchase_invoices_in_month", r);
+                    let invoices = []
+                    for (let i of r.message) {
+                        invoices.push({ purchase_invoice: i })
+                    }
+                    frm.set_value("purchase_billing_line", invoices)
+                    frm.set_value("invoice_count", invoices.length)
+                    frm.refresh_field('purchase_billing_line');
+                }
+            });
+        }
     }
+
 });
 
-function make_journal_entry(frm) {    
+function make_journal_entry(frm) {
     frappe.call({
         method: "make_journal_entry",
         doc: frm.doc,
-        callback: function(r) {
+        callback: function (r) {
             if (r.message) {
-                var doclist = frappe.model.sync(r.message);                
+                var doclist = frappe.model.sync(r.message);
                 frappe.set_route("Form", doclist[0].doctype, doclist[0].name);
             }
         }
