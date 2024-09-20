724c80b608a6798c802d40804f212a7a8b5ede5c
yot-anan-gj <yot-anan@goingjesse.com>
2024-09-20 05:06:55 +0000

    [Edited] : Refactor custom_purchase_billing.py
    - Import necessary modules and functions
    - Add new method to create Payment Entry
    - Set payment details and references in Payment Entry
    - Improve code readability and organization

--- lpp/custom/custom_purchase_billing.py
+++ lpp/custom/custom_purchase_billing.py
@@ -1,6 +1,7 @@
+from sqlite3 import Date
 import frappe
 from purchase_billing.purchase_billing.doctype.purchase_billing.purchase_billing import PurchaseBilling
-from frappe.utils import get_first_day, get_last_day
+from frappe.utils import get_first_day, get_last_day , now
 
 class CustomPurchaseBilling(PurchaseBilling):      
     @frappe.whitelist()
@@ -17,6 +18,28 @@ class CustomPurchaseBilling(PurchaseBilling):
             journal_entry.due_date = purchase_billing.threshold_date
 
         return journal_entry
+    
+    @frappe.whitelist()
+    def make_payment_entry(self):
+        # สร้าง Payment Entry
+        purchase_billing = self
+        payment_entry = frappe.new_doc("Payment Entry")
+        payment_entry.payment_type = "Pay"
+        payment_entry.posting_date = now()
+        payment_entry.party_type = "Supplier"
+        payment_entry.party = purchase_billing.supplier
+        payment_entry.party_name = purchase_billing.supplier_name
+        payment_entry.paid_amount = purchase_billing.total_outstanding_amount
+        
+        # สร้าง Payment Entry Reference
+        payment_entry_reference = frappe.new_doc("Payment Entry Reference")
+        payment_entry_reference.reference_doctype = "Purchase Billing"
+        payment_entry_reference.reference_name = purchase_billing.name
+        payment_entry_reference.total_amount = purchase_billing.total_billing_amount
+        payment_entry_reference.outstanding_amount = purchase_billing.total_outstanding_amount
+        payment_entry.append("references", payment_entry_reference)
+
+        return payment_entry
     
     @frappe.whitelist()
     def get_purchase_invoices_in_month(self, supplier, currency, threshold_type, threshold_date, tax_type = None):
