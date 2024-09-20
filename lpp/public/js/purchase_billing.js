169080830e3a27bf9a1c7fdd0b11a87bc3533f47
yot-anan-gj <yot-anan@goingjesse.com>
2024-09-20 05:08:19 +0000

    [Added] :  add create payment entry from purchase billing

--- lpp/public/js/purchase_billing.js
+++ lpp/public/js/purchase_billing.js
@@ -3,6 +3,9 @@ frappe.ui.form.on('Purchase Billing', {
         frm.add_custom_button(__('Create Journal Entry'), function () {
             make_journal_entry(frm);
         });
+        frm.add_custom_button(__('Create Payment Entry'), function () {
+            make_payment_entry(frm);
+        });
     },
     get_purchase_invoices: function (frm) {
         
@@ -35,6 +38,19 @@ frappe.ui.form.on('Purchase Billing', {
 function make_journal_entry(frm) {
     frappe.call({
         method: "make_journal_entry",
+        doc: frm.doc,
+        callback: function (r) {
+            if (r.message) {
+                var doclist = frappe.model.sync(r.message);
+                frappe.set_route("Form", doclist[0].doctype, doclist[0].name);
+            }
+        }
+    });
+}
+
+function make_payment_entry(frm) {    
+    frappe.call({
+        method: "make_payment_entry",
         doc: frm.doc,
         callback: function (r) {
             if (r.message) {
