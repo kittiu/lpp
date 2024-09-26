0b1456d9e5c76085582a55c8d65b59646b87ce05
PanApiwana <apiwana@goingjesse.com>
2024-09-26 07:57:30 +0000

    [Edited]: Work Order
    - แก้ไข Jobcard Remaining

--- lpp/custom/work_order.py
+++ lpp/custom/work_order.py
@@ -14,10 +14,15 @@ def get_jobcard_remaining(data):
         # Parse the string as JSON
         data = json.loads(data)
 
+    job_cards = frappe.db.get_all('Job Card', 
+        filters={'work_order': data['name']}, 
+        fields=['custom_runcard_no',],
+        group_by='custom_runcard_no'
+    )
+
     count_amount = data['custom_total_run_cards']
-    count_jobcard = frappe.db.count('Job Card', {'work_order': data['name']})
-    count_operation = frappe.db.count('Work Order Operation', {'parent': data['name']})
-    result = count_amount - (count_jobcard / count_operation)
+    count_jobcard = count_distinct_runcard_no(job_cards)
+    result = count_amount - count_jobcard
 
     return result
 
