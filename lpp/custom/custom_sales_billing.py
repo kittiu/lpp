2390fdbc0940a08d6c43fe713ec786a3ae2464ca
PanApiwana <apiwana@goingjesse.com>
2024-10-01 10:45:23 +0000

    [Fixed]: Payment Entry
    - Create Payment Entry มาจากหน้า Purchase Billing, Sales Billing ไม่สามารถบันทึกได้ระบบแจ้งว่า Fields Recived Amount is a mandatory

--- lpp/custom/custom_sales_billing.py
+++ lpp/custom/custom_sales_billing.py
@@ -20,6 +20,7 @@ class CustomSalesBilling(SalesBilling):
     
     @frappe.whitelist()
     def make_payment_entry(self):
+        print('make_payment_entry', self)
         # สร้าง Payment Entry
         purchase_billing = self
         payment_entry = frappe.new_doc("Payment Entry")
@@ -29,7 +30,8 @@ class CustomSalesBilling(SalesBilling):
         payment_entry.party = purchase_billing.customer
         payment_entry.party_name = purchase_billing.customer_name
         payment_entry.paid_amount = purchase_billing.total_outstanding_amount
-        
+        payment_entry.received_amount = purchase_billing.total_outstanding_amount
+
         # สร้าง Payment Entry Reference
         payment_entry_reference = frappe.new_doc("Payment Entry Reference")
         payment_entry_reference.reference_doctype = "Sales Billing"
