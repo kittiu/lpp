f8a0a587013ff7207d57c3ed2a4c3246e68481e8
daris-dev <daris@goingjesse.com>
2024-10-02 09:53:04 +0000

    [Edited]: Customize  pricing rule - ฟิลด์ Title ใช้ชื่อเดียวกับฟิลด์ Naming - เอาฟิลด์ warehouse ออก - ฟิลด์ Discount on Other Item  > Apply Rule On Other เอาออก - ฟิลด์ Company , Currency ซ่อน - เอาฟิลด์ Min Amt, Max Amt ออก

--- lpp/public/js/pricing_rule.js
+++ lpp/public/js/pricing_rule.js
@@ -1,7 +1,22 @@
 frappe.ui.form.on("Pricing Rule", {
     refresh(frm) {
-        if(!frm.doc.valid_upto){
+        // Set the default value for 'valid_upto' if it's empty
+        if (!frm.doc.valid_upto) {
             frm.set_value("valid_upto", frappe.datetime.add_months(frm.doc.transaction_date, 12));
         }
+
+        // Hide the 'currency' field
+        frm.set_df_property('currency', 'hidden', 1);
+
+        // Update the title based on naming_series
+        frm.trigger('update_title');
+    },
+
+    naming_series(frm) {
+        frm.trigger('update_title');
+    },
+
+    update_title(frm) {
+        frm.set_value("title", frm.doc.naming_series);
     }
-})
+});
