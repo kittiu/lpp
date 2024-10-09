1abb4b33c7c855ebbef76386edf814e63b647366
PanApiwana <apiwana@goingjesse.com>
2024-10-09 09:43:25 +0000

    [Edited]: Job Card
    - #143 Set ให้การส่งของระหว่าง Station จำนวนสินค้า station  ใหม่ ต้อง <= จำนวนสินค้า station ก่อนหน้า //ยกเว้น Station สุดท้าย Job card แรก สามารถผลิตเกินได้

--- lpp/public/js/job_card.js
+++ lpp/public/js/job_card.js
@@ -1,9 +1,47 @@
+
 // Trigger events for the Job Card doctype
 frappe.ui.form.on("Job Card", {
     refresh(frm) {
         // Set 'item_code' and 'item_name' fields in 'scrap_items' child table to read-only
         frm.get_field("scrap_items").grid.toggle_enable("item_code", false);
         frm.get_field("scrap_items").grid.toggle_enable("item_name", false);
+
+        setTimeout(() => {
+            if (frm.custom_buttons && frm.custom_buttons[('Complete Job')]) {
+                // Remove Original Button
+                frm.remove_custom_button(__('Complete Job'))
+                frm.add_custom_button(__("Complete Job"), () => {
+                    var sub_operations = frm.doc.sub_operations;
+
+                    let set_qty = true;
+                    if (sub_operations && sub_operations.length > 1) {
+                        set_qty = false;
+                        let last_op_row = sub_operations[sub_operations.length - 2];
+    
+                        if (last_op_row.status == "Complete") {
+                            set_qty = true;
+                        }
+                    }
+    
+                    if (set_qty) {
+                        frappe.prompt(
+                            {
+                                fieldtype: "Float",
+                                label: __("Completed Quantity"),
+                                fieldname: "qty",
+                                default: frm.doc.for_quantity,
+                            },
+                            (data) => {
+                                frm.events.custom_complete_job(frm, "Complete", data.qty);
+                            },
+                            __("Enter Value")
+                        );
+                    } else {
+                        frm.events.custom_complete_job(frm, "Complete", 0.0);
+                    }
+                }).addClass("btn-primary");
+            }
+        }, 10);
     },
     // Triggered when 'production_item' changes
     production_item(frm) {
@@ -12,7 +50,50 @@ frappe.ui.form.on("Job Card", {
     // Triggered when 'custom_production_item_name' changes
     custom_production_item_name(frm) {
         update_scrap_items(frm);
-    }
+    },
+    custom_complete_job: async function (frm, status, completed_qty) {
+		const args = {
+			job_card_id: frm.doc.name,
+			complete_time: frappe.datetime.now_datetime(),
+			status: status,
+			completed_qty: completed_qty,
+		};
+
+        const current_operation_no = frm.doc.custom_operation_no;
+        const total_operations = frm.doc.custom_total_operation;
+
+        // Check if custom_operation_no is neither the first nor the last
+        if (current_operation_no !== '1' && current_operation_no !== total_operations) {
+            const previous_operation_no = current_operation_no - 1;
+
+            const filters = {
+                work_order: frm.doc.work_order,
+                custom_runcard_no: frm.doc.custom_runcard_no,
+                status: "Completed",
+                custom_operation_no: previous_operation_no
+            };
+
+            // Fetch Job Card entries that match the filter criteria
+            const result = await frappe.db.get_list('Job Card', {
+                fields: ['total_completed_qty'],
+                filters: filters
+            });
+            
+            if (result.length > 0) {
+                const previous_total_completed_qty = result[0].total_completed_qty;
+                // Check if current completed quantity exceeds the previous operation's total completed quantity
+                if ((frm.doc.total_completed_qty + completed_qty) > previous_total_completed_qty) {
+                    return frappe.msgprint({
+                        title: __('Error'),
+                        message: __('ไม่สามารถระบุจำนวน FG ได้เกินกว่าจำนวน FG ของ Operation ก่อนหน้า'),
+                        indicator: 'red'
+                    });
+                }
+            }
+        }
+        // If the operation passes the checks or is the first/last operation, make the time log
+        frm.events.make_time_log(frm, args);
+    },
 });
 
 // Function to update 'item_code' and 'item_name' in all rows of 'scrap_items' child table
