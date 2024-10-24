04bc9fea48a8e48f6e7f37181f2a3ad53742b0e4
yot-anan-gj <yot-anan@goingjesse.com>
2024-10-24 10:33:17 +0000

    [Edited] :  ลบโค้ดส่วนที่ไม่ได้ใช้ที่เกี่ยวข้องกับตารางเก่า

--- lpp/public/js/quality_inspection.js
+++ lpp/public/js/quality_inspection.js
@@ -1,131 +1,8 @@
-const type_check_box = [
-    "specification",
-    "custom_item_1",
-    "custom_item_2",
-    "custom_item_3",
-    "custom_item_4",
-    "custom_item_5",
-    "custom_item_6",
-    "custom_item_7",
-    "custom_item_8",
-    "custom_item_9",
-]
-
-const type_reading = [
-    "specification",
-    "reading_1",
-    "reading_2",
-    "reading_3",
-    "reading_4",
-    "reading_5",
-    "reading_6",
-    "reading_7",
-    "reading_8",
-    "reading_9",
-]
-
-const type_accept_reject = [
-    "specification",
-    "custom_accept__reject",
-    "custom_quantity",
-    "custom_qty",
-]
-
-const type_dimension_report = [
-    "specification",
-    "reading_1",
-    "reading_2",
-    "reading_3",
-    "reading_4",
-    "reading_5",
-    "custom_accept__reject",
-]
-
-const type_specifications_report = [
-    "specification",
-    "reading_1",
-    "reading_2",
-    "reading_3",
-    "reading_4",
-    "reading_5",
-    "custom_average_",
-    "custom_accept__reject",
-]
-
-// Reusable function to setup user-defined columns
-function setupUserDefinedColumns(frm, tableField, typeArray) {
-    frm.fields_dict[tableField].grid.setup_user_defined_columns = function () {
-        if (typeArray && typeArray.length) {
-            frm.fields_dict[tableField].grid.user_defined_columns = typeArray.map(fieldname => {
-                let column = frappe.meta.get_docfield('Quality Inspection Reading', fieldname);
-                if (column) {
-                    column.in_list_view = 1;
-                    column.columns = 1;
-                    return column;
-                }
-            }).filter(Boolean);
-        }
-    };
-    frm.fields_dict[tableField].grid.setup_user_defined_columns();
-    frm.fields_dict[tableField].grid.refresh();
-    frm.refresh_field(tableField);
-}
-
 frappe.ui.form.on("Quality Inspection", {
     onload: function (frm) {
 
-        // ** Tab IMQT
-        // Setup for custom_quality_inspection_checkbox_1_table
-        setupUserDefinedColumns(frm, 'custom_quality_inspection_checkbox_1_table', type_check_box);
-
-        // Setup for custom_quality_inspection_freetext_1_template_table
-        setupUserDefinedColumns(frm, 'custom_quality_inspection_freetext_1_template_table', type_reading);
-
-        // Setup for custom_quality_inspection_freetext_1_template_table
-        setupUserDefinedColumns(frm, 'custom_quality_inspection_checkbox_2_table', type_reading);
-
-        // Setup for custom_quality_inspection_template_table_1
-        setupUserDefinedColumns(frm, 'custom_quality_inspection_template_table_1', type_check_box);
-
-        // ** Tab BuyOff
-        // Setup for custom_buyoff_table_1
-        setupUserDefinedColumns(frm, 'custom_buyoff_table_1', type_specifications_report);
-
-        // Setup for custom_buyoff_table_2
-        setupUserDefinedColumns(frm, 'custom_buyoff_table_2', type_accept_reject);
-
-        // Setup for custom_buyoff_table_3
-        setupUserDefinedColumns(frm, 'custom_buyoff_table_3', type_accept_reject);
-
-        // Setup for custom_buyoff_table_4
-        setupUserDefinedColumns(frm, 'custom_buyoff_table_4', type_dimension_report);
-
-
-
-
-        // ** Tab Roving
-        // Setup for custom_roving_table_1
-        setupUserDefinedColumns(frm, 'custom_roving_table_1', type_accept_reject);
-
-        // Setup for custom_roving_table_2
-        setupUserDefinedColumns(frm, 'custom_roving_table_2', type_accept_reject);
-
-        // Setup for custom_roving_table_3
-        setupUserDefinedColumns(frm, 'custom_roving_table_3', type_accept_reject);
-
-
-        // ** Tab Final Inspection
-        // Setup for readings
-        setupUserDefinedColumns(frm, 'readings', type_accept_reject);
-
-        // Setup for readings
-        setupUserDefinedColumns(frm, 'custom_quality_inspection_template_2_table', type_accept_reject);
-
-        // Setup for custom_quality_inspection_template_3_table
-        setupUserDefinedColumns(frm, 'custom_quality_inspection_template_3_table', type_reading);
-
     },
-    refresh(frm) {
+    refresh: function (frm) {
         // Set ค่า default ของ inspection_type เป็น In Process
         if (!frm.doc.inspection_type) {
             frm.set_value('inspection_type', 'In Process');
@@ -137,26 +14,26 @@ frappe.ui.form.on("Quality Inspection", {
 
     },
     custom_inspection_progress: function (frm) {
-        if(frm.doc.custom_inspection_progress === 'Buyoff'){
+        if (frm.doc.custom_inspection_progress === 'Buyoff') {
             frm.set_value('custom_buyoff_inspect_date', frappe.datetime.now_datetime());
             frm.set_value('custom_roving_inspect_date', null);
             frm.set_value('custom_final_inspection_inspect_date', null);
-        }else if(frm.doc.custom_inspection_progress === 'Roving'){
+        } else if (frm.doc.custom_inspection_progress === 'Roving') {
             frm.set_value('custom_buyoff_inspect_date', null);
             frm.set_value('custom_roving_inspect_date', frappe.datetime.now_datetime());
             frm.set_value('custom_final_inspection_inspect_date', null);
-        }else if(frm.doc.custom_inspection_progress === 'Final Inspection'){
+        } else if (frm.doc.custom_inspection_progress === 'Final Inspection') {
             frm.set_value('custom_buyoff_inspect_date', null);
             frm.set_value('custom_roving_inspect_date', null);
             frm.set_value('custom_final_inspection_inspect_date', frappe.datetime.now_datetime());
-        }else{
+        } else {
             frm.set_value('custom_buyoff_inspect_date', null);
             frm.set_value('custom_roving_inspect_date', null);
             frm.set_value('custom_final_inspection_inspect_date', null);
         }
     },
     reference_type: function (frm) {
-        if(frm.doc.reference_type && frm.doc.reference_name && !frm.doc.custom_supplier){
+        if (frm.doc.reference_type && frm.doc.reference_name && !frm.doc.custom_supplier) {
             frm.events.get_supplier(frm);
         }
     },
@@ -171,9 +48,9 @@ frappe.ui.form.on("Quality Inspection", {
         } else if (frm.doc.reference_type && frm.doc.reference_name && !frm.doc.custom_supplier) {
             frm.events.get_supplier(frm);
         }
-    },    
-    get_supplier:function (frm) {
-        if(frm.doc.reference_type === 'Purchase Receipt' && frm.doc.reference_name && !frm.doc.custom_supplier){
+    },
+    get_supplier: function (frm) {
+        if (frm.doc.reference_type === 'Purchase Receipt' && frm.doc.reference_name && !frm.doc.custom_supplier) {
             const pr_name = frm.doc.reference_name;
             frappe.db.get_value('Purchase Receipt', pr_name, "supplier", function (value) {
                 frm.set_value('custom_supplier', value['supplier']);
@@ -236,143 +113,7 @@ frappe.ui.form.on("Quality Inspection", {
                 },
             });
         }
-    },
-    custom_quality_inspection_template_2: function (frm) {
-        if (frm.doc.custom_quality_inspection_template_2) {
-            return frm.call({
-                method: "custom_get_item_specification_details",
-                doc: frm.doc,
-                args: {
-                    template_key: "custom_quality_inspection_template_2",
-                    table_key: "custom_quality_inspection_template_2_table"
-                },
-                callback: function () {
-                    refresh_field("custom_quality_inspection_template_2_table");
-                },
-            });
-        }
-    },
-    custom_quality_inspection_template_3: function (frm) {
-        if (frm.doc.custom_quality_inspection_template_3) {
-            return frm.call({
-                method: "custom_get_item_specification_details",
-                doc: frm.doc,
-                args: {
-                    template_key: "custom_quality_inspection_template_3",
-                    table_key: "custom_quality_inspection_template_3_table"
-                },
-                callback: function () {
-                    refresh_field("custom_quality_inspection_template_3_table");
-                },
-            });
-        }
-    },
-    custom_quality_inspection_checkbox_1_template_: function (frm) {
-        if (frm.doc.custom_quality_inspection_checkbox_1_template_) {
-            return frm.call({
-                method: "custom_get_item_specification_details",
-                doc: frm.doc,
-                args: {
-                    template_key: "custom_quality_inspection_checkbox_1_template_",
-                    table_key: "custom_quality_inspection_checkbox_1_table"
-                },
-                callback: function () {
-                    refresh_field("custom_quality_inspection_checkbox_1_table");
-                },
-            });
-        }
-    },
-    custom_quality_inspection_freetext_1_template_: function (frm) {
-        if (frm.doc.custom_quality_inspection_freetext_1_template_) {
-            return frm.call({
-                method: "custom_get_item_specification_details",
-                doc: frm.doc,
-                args: {
-                    template_key: "custom_quality_inspection_freetext_1_template_",
-                    table_key: "custom_quality_inspection_freetext_1_template_table"
-                },
-                callback: function () {
-                    refresh_field("custom_quality_inspection_freetext_1_template_table");
-                },
-            });
-        }
-    },
-    custom_quality_inspection_checkbox_2: function (frm) {
-        if (frm.doc.custom_quality_inspection_checkbox_2) {
-            return frm.call({
-                method: "custom_get_item_specification_details",
-                doc: frm.doc,
-                args: {
-                    template_key: "custom_quality_inspection_checkbox_2",
-                    table_key: "custom_quality_inspection_checkbox_2_table"
-                },
-                callback: function () {
-                    refresh_field("custom_quality_inspection_checkbox_2_table");
-                },
-            });
-        }
-    },
-    custom_quality_inspection_template_1: function (frm) {
-        if (frm.doc.custom_quality_inspection_template_1) {
-            return frm.call({
-                method: "custom_get_item_specification_details",
-                doc: frm.doc,
-                args: {
-                    template_key: "custom_quality_inspection_template_1",
-                    table_key: "custom_quality_inspection_template_table_1"
-                },
-                callback: function () {
-                    refresh_field("custom_quality_inspection_template_table_1");
-                },
-            });
-        }
-    },
-    custom_roving_1: function (frm) {
-        if (frm.doc.custom_roving_1) {
-            return frm.call({
-                method: "custom_get_item_specification_details",
-                doc: frm.doc,
-                args: {
-                    template_key: "custom_roving_1",
-                    table_key: "custom_roving_table_1"
-                },
-                callback: function () {
-                    refresh_field("custom_roving_table_1");
-                },
-            });
-        }
-    },
-    custom_roving_2: function (frm) {
-        if (frm.doc.custom_roving_2) {
-            return frm.call({
-                method: "custom_get_item_specification_details",
-                doc: frm.doc,
-                args: {
-                    template_key: "custom_roving_2",
-                    table_key: "custom_roving_table_2"
-                },
-                callback: function () {
-                    refresh_field("custom_roving_table_2");
-                },
-            });
-        }
-    },
-    custom_roving_3: function (frm) {
-        if (frm.doc.custom_roving_3) {
-            return frm.call({
-                method: "custom_get_item_specification_details",
-                doc: frm.doc,
-                args: {
-                    template_key: "custom_roving_3",
-                    table_key: "custom_roving_table_3"
-                },
-                callback: function () {
-                    refresh_field("custom_roving_table_3");
-                },
-            });
-        }
     }
-
 });
 
 frappe.ui.form.on('Quality Inspection Reading', {
@@ -462,3 +203,23 @@ frappe.ui.form.on('Quality Inspection Reading', {
     }
 });
 
+
+frappe.ui.form.on("Quality Inspection Order", {
+    form_render(frm, cdt, cdn) {
+        const sample_size = frm.doc.sample_size;
+        
+        for (let i = 1; i <= 32; i++) {
+            if (i <= sample_size) {
+                frm.fields_dict.custom_quality_inspection_order_table_1.grid.update_docfield_property('inspected_value_' + i, "hidden", 0);
+                frm.fields_dict.custom_quality_inspection_order_table_1.grid.update_docfield_property('approval_' + i, "hidden", 0);
+                frm.fields_dict.custom_quality_inspection_order_table_1.grid.update_docfield_property('remark_' + i, "hidden", 0);
+
+            } else {
+                frm.fields_dict.custom_quality_inspection_order_table_1.grid.update_docfield_property('inspected_value_' + i, "hidden", 1);
+                frm.fields_dict.custom_quality_inspection_order_table_1.grid.update_docfield_property('approval_' + i, "hidden", 1);
+                frm.fields_dict.custom_quality_inspection_order_table_1.grid.update_docfield_property('remark_' + i, "hidden", 1);
+            }
+        }
+
+    }
+})
